a70b5c956ce6cc20289d1a91f46156873ac8cc2d
WillPernalonga <wilianpernalonga171@gmail.com>
2024-09-09 22:40:32 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,29 @@
+let dados = [
+    {titulo: "Vanessa Bravo",
+    descricao: "Vanessa Bravo : é uma colaboradora exemplar. Sua capacidade de trabalhar em equipe e compartilhar seus conhecimentos com os colegas contribui significativamente para o sucesso dos projetos.",
+    link : "https://www.instagram.com/vanbbravo/",
+    tags : "borboleta butterfly linda gostosa"
+},
+{titulo: "Wilian Bravo",
+    descricao: "Wilian Bravo : Seu domínio das linguagens de programação é impressionante! A forma como ele soluciona o problema é extremamente eficiente e demonstra um profundo conhecimento da lógica de programação.",
+    link : "https://www.instagram.com/wilianbravo/",
+    tags : "Pernalonga bonitao lindao gostosao gordo"
+}, 
+{titulo: "Felipe Batista",
+    descricao: "Felipe Batista : é um aluno proativo e autodidata. Ele está sempre buscando aprimorar seus conhecimentos e explorar novas tecnologias, o que o torna um profissional muito valioso.",
+    link : "https://www.instagram.com/fe_b3rnardino/",
+    tags : "viado feh boiola bombado"
+},
+{titulo: "Ana Carolina",
+    descricao: "Ana Carolina : possui uma mente analítica e lógica que o auxilia a encontrar soluções criativas para os problemas de programação.",
+    link : "https://www.instagram.com/curor_/",
+    tags : "curor curolzinha carolzinha aninha"
+},   
+{titulo: "Maria Eduarda",
+    descricao: "Maria Eduarda : possui um grande potencial para se tornar uma programadora de sucesso. Seu futuro na área é promissor.",
+    link : "https://www.instagram.com/_.dud4w/",
+    tags : "duda dudinha madu "
+},
+];
+
+    
